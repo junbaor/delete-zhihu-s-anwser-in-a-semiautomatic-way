3d07edcd9f52843b9167edee0ecd6b50ce594394
junbaor <junbaor@gmail.com>
2021-05-22 22:45:02 +0800

    Update index.js
    原脚本已不可用, 做了一下修改，亲测可行

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 setInterval(function(){
     // 打开菜单
-    document.getElementsByClassName('Popover ContentItem-action')[2].childNodes[0].click();
+    document.getElementsByClassName("Popover ContentItem-action")[1].children[0].click()
 
     //点击删除按钮
-    var menu = document.getElementsByClassName('AnswerItem-selfMenu')[0];var item = menu.childNodes;item[item.length-1].click();
+    document.getElementsByClassName("Button Menu-item AnswerItem-selectMenuItem Button--plain")[7].click()
 
     //确认删除
     setTimeout(function() {
-        document.getElementsByClassName('ModalButtonGroup')[0].childNodes[0].click();
+        document.getElementsByClassName("Button Button--primary Button--blue")[1].click()
     },1000)
 
 }, 2000)
